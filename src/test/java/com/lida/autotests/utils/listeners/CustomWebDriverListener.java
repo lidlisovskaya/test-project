e1c0bd8474bd0d2107b0aa21594e6323f9b0da28
Lidziya Lisouskaya <lidziya_lisouskaya@epam.com>
2025-03-21 15:10:27 +0400

    improvement of allure report

--- src/test/java/com/lida/autotests/utils/listeners/CustomWebDriverListener.java
+++ src/test/java/com/lida/autotests/utils/listeners/CustomWebDriverListener.java
@@ -1,11 +1,14 @@
 package com.lida.autotests.utils.listeners;
 
+import io.qameta.allure.Allure;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
+
+import static java.lang.String.format;
 
 @Log4j2
 public class CustomWebDriverListener implements WebDriverListener {
@@ -23,11 +26,13 @@ public class CustomWebDriverListener implements WebDriverListener {
     @Override
     public void afterTo(WebDriver.Navigation navigation, String url) {
         log.info("Navigated to URL: {}", url);
+        Allure.step(STR."Navigated to URL: \{url}");
     }
 
     @Override
     public void afterBack(WebDriver.Navigation navigation) {
         log.info("Navigated back.");
+        Allure.step("Navigated back.");
     }
 
     @Override
@@ -38,46 +43,55 @@ public class CustomWebDriverListener implements WebDriverListener {
     @Override
     public void afterRefresh(WebDriver.Navigation navigation) {
         log.info("Page refreshed.");
+        Allure.step("Page refreshed.");
     }
 
     @Override
     public void beforeFindElement(WebDriver webDriver, By by) {
         log.debug("Finding element: {}", by);
+        Allure.step(STR."Finding element \{by.toString()}");
     }
 
     @Override
     public void afterFindElement(WebDriver webDriver, By by, WebElement result) {
         log.info("Element found: {}", by);
+        Allure.step(STR."Element found: \{by.toString()}");
     }
 
     @Override
     public void beforeClick(WebElement element) {
         log.debug("Attempting to click: {}", getLocator(element));
+        Allure.step(STR."Attempting to click: \{getLocator(element)}");
     }
 
     @Override
     public void afterClick(WebElement element) {
         log.info("Clicked on element: {}", getLocator(element));
+        Allure.step(STR."Clicked on element: \{getLocator(element)}");
     }
 
     @Override
     public void beforeSendKeys(WebElement element, CharSequence[] keysToSend) {
         log.debug("Attempting to send keys '{}' to: {}", String.join("", keysToSend), getLocator(element));
+        Allure.step(format("Attempting to send keys %s to: %s", String.join("", keysToSend), getLocator(element)));
     }
 
     @Override
     public void afterSendKeys(WebElement element, CharSequence[] keysToSend) {
         log.info("Typed '{}' into element: {}", String.join("", keysToSend), getLocator(element));
+        Allure.step(format("Typed %s into element: %s", String.join("", keysToSend), getLocator(element)));
     }
 
     @Override
     public void beforeGetText(WebElement element) {
         log.debug("Getting text from element: {}", getLocator(element));
+        Allure.step(STR."Getting text from element: \{getLocator(element)}");
     }
 
     @Override
     public void afterGetText(WebElement element, String text) {
         log.info("Extracted text '{}' from element: {}", text, getLocator(element));
+        Allure.step(format("Extracted text %s from element: %s", text, getLocator(element)));
     }
 
     private String getLocator(WebElement element) {
